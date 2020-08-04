fd775f10da68d7e460517b675f607c8a80cc8a3d
Felipe Martins de Jesus <felipe.jesus@presidencia.gov.br>
2020-08-04 14:07:22 -0300

    Criado o componente TeacherItem

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -29,31 +28,11 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars3.githubusercontent.com/u/4887117?s=460&u=36aafd594c3934388623cb502c633784f610cd2c&v=4" alt="Felipe Martins" />
-            <div>
-              <strong>Felipe Martins</strong>
-              <span>Química</span>
-            </div>
-          </header>
-          <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br />
-            <br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-          </p>
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 120,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="WhatsApp" />
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   );
