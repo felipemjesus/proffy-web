94e6313cf2d477c20f44b1aefca4757fdab5fcef
Felipe Martins <felipemjesus@outlook.com>
2020-08-06 22:16:10 -0300

    Criado o componente de Input

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -7,7 +7,8 @@ import backIcon from '../../assets/images/icons/back.svg';
 import './styles.css';
 
 interface PageHeaderProps {
-    title: string;
+  title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => (
@@ -21,6 +22,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => (
 
     <div className="header-content">
       <strong>{props.title}</strong>
+      { props.description && <p>{props.description}</p> }
 
       {props.children}
     </div>
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -3,14 +3,25 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
+import Input from '../../components/Input';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo, é preencher esse formulário de inscrição."
+      />
 
       <main>
-        
+        <form action="#">
+          <fieldset>
+            <legend>Seus dados</legend>
+            <Input name="name" label="Nome completo" />
+            <Input name="avatar" label="Avatar" />
+            <Input name="whatsapp" label="WhatsApp" />
+          </fieldset>
+        </form>
       </main>
     </div>
   );
