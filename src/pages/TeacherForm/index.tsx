ef18bb3fc000dd34ca2f33427557de16ba874133
Felipe Martins <felipemjesus@outlook.com>
2020-08-06 22:53:58 -0300

    Criado o componente para o Textarea

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
-import Input from '../../components/Input';
 
 function TeacherForm() {
   return (
@@ -14,13 +17,31 @@ function TeacherForm() {
       />
 
       <main>
-        <form action="#">
+        <form>
           <fieldset>
             <legend>Seus dados</legend>
             <Input name="name" label="Nome completo" />
             <Input name="avatar" label="Avatar" />
             <Input name="whatsapp" label="WhatsApp" />
+            <Textarea name="bio" label="Biografia" />
           </fieldset>
+
+          <fieldset>
+            <legend>Sobre a aula</legend>
+            <Input name="subject" label="Matéria" />
+            <Input name="cost" label="Custo da sua hora por aula" />
+          </fieldset>
+
+          <footer>
+            <p>
+              <img src={warningIcon} alt="Aviso importante" />
+              Importante! <br />
+              Preencha todos os dados
+            </p>
+            <button type="button">
+              Salvar cadastro
+            </button>
+          </footer>
         </form>
       </main>
     </div>
